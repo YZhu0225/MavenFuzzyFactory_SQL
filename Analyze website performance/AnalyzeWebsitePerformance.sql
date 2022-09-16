254c53e1d92ddd55da744b91c23cb0af7e0acdae
Yuanjing Zhu <110933007+YZhu0225@users.noreply.github.com>
2022-09-16 19:39:03 -0400

    Update AnalyzeWebsitePerformance.sql

--- Analyze website performance/AnalyzeWebsitePerformance.sql
+++ Analyze website performance/AnalyzeWebsitePerformance.sql
@@ -1,3 +1,67 @@
 /*
-
+Could you help me get my head around the site by pulling the most-viewed website pages, ranked by session volume?
 */
+
+
+
+
+
+
+/*
+Would you be able to pull a list of the top entry pages? I want to confirm where our users are hitting the site. 
+If you could pull all entry pages and rank them on entry volume, that would be great.
+*/
+
+
+
+
+
+/* 
+The other day you showed us that all of our traffic is landing on the homepage right now. We should check how that landing page is performing. 
+Can you pull bounce rates for traffic landing on the homepage? I would like to see three numbers…Sessions, Bounced Sessions, and % of Sessions which Bounced 
+(aka “Bounce Rate”).
+*/
+
+
+
+
+/*
+Based on your bounce rate analysis, we ran a new custom landing page (/lander-1) in a 50/50 test against the homepage (/home) for our gsearch nonbrand traffic. 
+Can you pull bounce rates for the two groups so we can evaluate the new page? Make sure to just look at the time period where /lander-1 was getting traffic, so that it is a fair 
+comparison
+*/
+
+
+
+
+
+/*
+Could you pull the volume of paid search nonbrand traffic landing on /home and /lander-1, trended weekly since June 1st? I want to confirm the traffic is all routed correctly.
+Could you also pull our overall paid search bounce rate trended weekly? I want to make sure the lander change has improved the overall picture.
+*/
+
+
+
+
+
+/*
+I’d like to understand where we lose our gsearch visitors between the new /lander-1 page and placing an order. Can you build us a full conversion funnel, analyzing how many 
+customers make it to each step? Start with /lander-1 and build the funnel all the way to our thank you page. Please use data since August 5th.
+*/
+
+
+
+
+/*
+We tested an updated billing page based on your funnel analysis. Can you take a look and see whether /billing-2 is doing any better than the original /billing page? 
+We’re wondering what % of sessions on those pages end up placing an order. FYI – we ran this test for all traffic, not just for our search visitors.
+*/
+
+
+
+
+
+
+
+
+
