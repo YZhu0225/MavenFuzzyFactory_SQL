5f3d9eda5bc0d11648a1126d1140d9391fc750ac
Yuanjing Zhu <110933007+YZhu0225@users.noreply.github.com>
2022-09-16 10:50:14 -0400

    Update ProductAnalysis.sql

--- Product analysis/ProductAnalysis.sql
+++ Product analysis/ProductAnalysis.sql
@@ -75,7 +75,7 @@ ON snp.min_next_pageview_id = wp.website_pageview_id;
 -- Step 4: summarize the data and analyze the pre vs post periods
 SELECT time_period,
 	   COUNT(DISTINCT website_session_id) AS sessions,
-       COUNT(DISTINCT CASE WHEN next_pageview_url IS NOT NULL THEN website_session_id ELSE NULL END) AS w_next_pg,
+      	   COUNT(DISTINCT CASE WHEN next_pageview_url IS NOT NULL THEN website_session_id ELSE NULL END) AS w_next_pg,
 	   COUNT(DISTINCT CASE WHEN next_pageview_url IS NOT NULL THEN website_session_id ELSE NULL END) / COUNT(DISTINCT website_session_id) AS pct_w_next_pg,
 	   COUNT(DISTINCT CASE WHEN next_pageview_url = "/the-original-mr-fuzzy" THEN website_session_id ELSE NULL END) AS to_mrfuzzy,
 	   COUNT(DISTINCT CASE WHEN next_pageview_url = "/the-original-mr-fuzzy" THEN website_session_id ELSE NULL END) / COUNT(DISTINCT website_session_id) AS pct_to_mrfuzzy,
@@ -110,7 +110,7 @@ ON wp.website_session_id = ssp.website_session_id
 
 -- Step 3: pull all pageviews and identify the funnel steps
 SELECT ssp.website_session_id,
-	   ssp.product_page_seen,      # original / love_bear
+       ssp.product_page_seen,      # original / love_bear
        CASE WHEN wp.pageview_url = "/cart" THEN 1 ELSE 0 END AS cart_page,
        CASE WHEN wp.pageview_url = "/shipping" THEN 1 ELSE 0 END AS shipping_page,
        CASE WHEN wp.pageview_url = "/billing-2" THEN 1 ELSE 0 END AS billing_page,
@@ -126,10 +126,10 @@ ORDER BY ssp.website_session_id, wp.created_at;
 CREATE TEMPORARY TABLE session_product_level_made_it_flags
 SELECT website_session_id,
 	   CASE
-		 WHEN product_page_seen = "/the-original-mr-fuzzy" THEN "mrfuzzy"
-         WHEN product_page_seen = "/the-forever-love-bear" THEN "lovebear"
-	     END AS product_seen,
-	   MAX(cart_page) AS cart_made_it,
+		WHEN product_page_seen = "/the-original-mr-fuzzy" THEN "mrfuzzy"
+		WHEN product_page_seen = "/the-forever-love-bear" THEN "lovebear"
+	   END AS product_seen,
+       MAX(cart_page) AS cart_made_it,
        MAX(shipping_page) AS shipping_made_it,
        MAX(billing_page) AS billing_made_it,
        MAX(thankyou_page) AS thankyou_made_it
@@ -173,6 +173,106 @@ FROM session_product_level_made_it_flags
 GROUP BY product_seen;
        
        
+
+/*
+On September 25th we started giving customers the option to add a 2nd product while on the /cart page. Morgan says this has been positive, but I’d like your take on it. Could you please compare the month before vs the month 
+after the change? I’d like to see CTR from the /cart page, Avg Products per Order, AOV, and overall revenue per /cart page view.
+*/
+
+USE mavenfuzzyfactory;
+
+-- STEP 1: Identify the relevant /cart pageviews and their sessions
+CREATE TEMPORARY TABLE sessions_seeing_cart
+SELECT CASE WHEN created_at < "2013-09-25" THEN "Pre_Cross_Sell"
+            WHEN created_at >= "2013-09-25" THEN "Post_Cross_Sell"
+		END AS time_period,
+		website_session_id AS cart_session_id,
+        website_pageview_id AS cart_pageview_id
+FROM website_pageviews
+WHERE created_at BETWEEN "2013-08-25" AND "2013-10-25" AND pageview_url = "/cart";
+
+
+-- STEP 2: See which of those /cart sessions clicked through to the shopping page
+CREATE TEMPORARY TABLE cart_sessions_seeing_another_page
+SELECT ssc.time_period,
+       ssc.cart_session_id,
+       MIN(wp.website_pageview_id) AS pv_id_after_cart    # get the next pageview_id after cart
+FROM sessions_seeing_cart AS ssc
+LEFT OUTER JOIN website_pageviews AS wp
+ON wp.website_session_id = ssc.cart_session_id 
+	AND wp.website_pageview_id > ssc.cart_pageview_id    # seeing another page after the /cart
+GROUP BY ssc.time_period, ssc.cart_session_id      
+HAVING MIN(wp.website_pageview_id) IS NOT NULL;
+
+
+-- STEP 3: Find the orders associated with the /car sessions. Analyze products purchased, ADV
+CREATE TEMPORARY TABLE sessions_orders
+SELECT ssc.time_period, 
+	   ssc.cart_session_id, 
+       o.order_id, 
+       o.items_purchased, 
+       o.price_usd
+FROM sessions_seeing_cart AS ssc
+JOIN orders AS o
+ON ssc.cart_session_id = o.website_session_id;
+
+
+-- STEP 4: Aggregate and analyze a summary of our findings.
+CREATE TEMPORARY TABLE full_data
+SELECT ssc.time_period,
+       ssc.cart_session_id,
+       CASE WHEN cssap.cart_session_id IS NULL THEN 0 ELSE 1 END AS clicked_to_another_page,
+       CASE WHEN so.order_id IS NULL THEN 0 ELSE 1 END AS placed_order,
+       so.items_purchased,
+       so.price_usd
+FROM sessions_seeing_cart AS ssc
+LEFT JOIN cart_sessions_seeing_another_page AS cssap
+	ON ssc.cart_session_id = cssap.cart_session_id
+LEFT JOIN sessions_orders AS so
+	ON ssc.cart_session_id = so.cart_session_id
+GROUP BY cart_session_id;
+
+SELECT time_period, 
+       COUNT(DISTINCT cart_session_id) AS cart_sessions,
+       SUM(clicked_to_another_page) AS clickthrouhgs,
+       SUM(clicked_to_another_page) / COUNT(DISTINCT cart_session_id) AS cart_ctr,
+       SUM(items_purchased) / SUM(placed_order) AS products_per_order,
+       SUM(price_usd) / SUM(placed_order) AS aov,                                  # AverageOrderValue
+       SUM(price_usd) / COUNT(DISTINCT cart_session_id) AS rev_per_cart_sesson     # revenue per cart session
+FROM full_data
+GROUP BY time_period;
+
+
+
+/*
+On December 12th 2013, we launched a third product targeting the birthday gift market (Birthday Bear). Could you please run a pre-post analysis comparing the month before vs. the month after, in terms of session-toorder conversion rate, AOV, products per order, and 
+revenue per session?
+*/
+
+USE mavenfuzzyfactory;
+
+SELECT CASE WHEN ws.created_at < "2013-12-12" THEN "Pre_Birthday_Bear" ELSE "Post_Birthday_Bear" END AS time_period,
+	   COUNT(DISTINCT order_id) / COUNT(DISTINCT ws.website_session_id) AS conv_rate,
+       SUM(price_usd) / COUNT(DISTINCT order_id) AS aov,    
+       SUM(items_purchased) / COUNT(DISTINCT order_id) AS products_per_order,
+       SUM(price_usd) / COUNT(DISTINCT ws.website_session_id) AS revenue_per_session
+FROM website_sessions AS ws
+LEFT JOIN orders AS o
+ON ws.website_session_id = o.website_session_id
+WHERE ws.created_at BETWEEN "2013-11-12" AND "2014-01-12"
+GROUP BY 1
+ORDER BY 1;
+
+
+
+/*
+Our Mr. Fuzzy supplier had some quality issues which weren’t corrected until September 2013. Then they had a major problem where the bears’ arms were falling off in 
+Aug/Sep 2014. As a result, we replaced them with a new supplier on September 16, 2014. Can you please pull monthly product refund rates, by product, and confirm our quality issues are now fixed?
+*/
+
+
+
+
 
 
 
