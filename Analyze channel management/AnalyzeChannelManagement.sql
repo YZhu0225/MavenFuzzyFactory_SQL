b8cef5a2fdfe38b50423b4e72b936fecd5c7ebda
Yuanjing Zhu <110933007+YZhu0225@users.noreply.github.com>
2022-09-16 19:41:46 -0400

    Update AnalyzeChannelManagement.sql

--- Analyze channel management/AnalyzeChannelManagement.sql
+++ Analyze channel management/AnalyzeChannelManagement.sql
@@ -1,1 +1,44 @@
+/*
+With gsearch doing well and the site performing better, we launched a second paid search channel, bsearch, around August 22.
+Can you pull weekly trended session volume since then and compare to gsearch nonbrand so I can get a sense for how important this will be for the business?
+*/
+
+
+
+
+/*
+I’d like to learn more about the bsearch nonbrand campaign. Could you please pull the percentage of traffic coming on Mobile, and compare that to gsearch? Feel free 
+to dig around and share anything else you find interesting. Aggregate data since August 22nd is great, no need to show trending at this point.
+*/
+
+
+
+
+/*
+I’m wondering if bsearch nonbrand should have the same bids as gsearch. Could you pull nonbrand conversion rates from session to order for gsearch and bsearch, and slice the 
+data by device type? Please analyze data from August 22 to September 18; we ran a special pre-holiday campaign for gsearch starting on September 19th, so the data after that isn’t fair game.
+*/
+
+
+
+
+/*
+Based on your last analysis, we bid down bsearch nonbrand on December 2nd.
+Can you pull weekly session volume for gsearch and bsearch nonbrand, broken down by device, since November 4th?
+If you can include a comparison metric to show bsearch as a percent of gsearch for each device, that would be great too.
+*/
+
+
+
+
+/*
+A potential investor is asking if we’re building any momentum with our brand or if we’ll need to keep relying on paid traffic.
+Could you pull organic search, direct type in, and paid brand search sessions by month, and show those sessions as a % of paid search nonbrand?
+*/
+
+
+
+
+
+
 
