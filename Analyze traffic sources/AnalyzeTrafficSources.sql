679553afa76b4d7783ec46e5aff102d51cbb5534
Yuanjing Zhu <110933007+YZhu0225@users.noreply.github.com>
2022-09-16 18:57:53 -0400

    Update AnalyzeTrafficSources.sql

--- Analyze traffic sources/AnalyzeTrafficSources.sql
+++ Analyze traffic sources/AnalyzeTrafficSources.sql
@@ -1,1 +1,44 @@
+/* 
+We've been live for almost a month now and we’re starting to generate sales. Can you help me understand where the bulk of our website sessions are coming from, 
+through yesterday? I’d like to see a breakdown by UTM source, campaign and referring domain if possible. Thanks!
+*/
+
+
+
+/*
+We've been live for almost a month now and we’re starting to generate sales. Can you help me understand where the bulk of our website sessions are coming 
+from, through yesterday? I’d like to see a breakdown by UTM source, campaign and referring domain if possible. Thanks!
+*/
+
+
+
+
+/*
+Based on your conversion rate analysis, we bid down gsearch nonbrand on 2012-04-15. Can you pull gsearch nonbrand trended session volume, by week, to see if the bid 
+changes have caused volume to drop at all?
+*/
+
+
+
+
+
+/*
+I was trying to use our site on my mobile device the other day, and the experience was not great. Could you pull conversion rates from session to order, by device type?
+If desktop performance is better than on mobile we may be able to bid up for desktop specifically to get more volume?
+*/
+
+
+
+
+
+/*
+After your device-level analysis of conversion rates, we realized desktop was doing well, so we bid our gsearch nonbrand desktop campaigns up on 2012-05-19. 
+Could you pull weekly trends for both desktop and mobile so we can see the impact on volume? You can use 2012-04-15 until the bid change as a baseline
+*/
+
+
+
+
+
+
 
